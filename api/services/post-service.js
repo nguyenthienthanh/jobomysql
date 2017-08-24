d94a7e456c19c53f4fd7442604377a746b96bd62
Thanh <nguyenthienthanh.kiyoshi@gmail.com>
2017-08-24 16:28:42 +0700

    Fixed mysql closed

--- api/services/post-service.js
+++ api/services/post-service.js
@@ -2,15 +2,10 @@ import mysql from 'mysql';
 import config from 'config';
 
 const mysqlConfig = config.get("mysql");
-const mysqlCon = mysql.createConnection({
-  host: mysqlConfig.host,
-  user: mysqlConfig.user,
-  password: mysqlConfig.pass,
-  database: mysqlConfig.database
-});
+
 const prefix = mysqlConfig.prefix;
 
-function connect() {
+function connect(mysqlCon) {
   return new Promise((resolve, reject) => {
     mysqlCon.connect(function (err) {
       if (err) reject(err);
@@ -19,16 +14,22 @@ function connect() {
   });
 }
 
-function close() {
+function close(mysqlCon) {
   mysqlCon.end();
 }
 
 export function get() {
   return new Promise((resolve, reject) => {
-    connect()
+    const mysqlCon = mysql.createConnection({
+      host: mysqlConfig.host,
+      user: mysqlConfig.user,
+      password: mysqlConfig.pass,
+      database: mysqlConfig.database
+    });
+    connect(mysqlCon)
       .then(status => {
         mysqlCon.query(`SELECT * FROM ${prefix}posts WHERE post_type LIKE 'post'`, (err, rows, fields) => {
-          close();
+          close(mysqlCon);
           if (err) reject(err);
           else {
             resolve(rows);
@@ -41,10 +42,16 @@ export function get() {
 
 export function getByName(postName) {
   return new Promise((resolve, reject) => {
-    connect()
+    const mysqlCon = mysql.createConnection({
+      host: mysqlConfig.host,
+      user: mysqlConfig.user,
+      password: mysqlConfig.pass,
+      database: mysqlConfig.database
+    });
+    connect(mysqlCon)
       .then(status => {
         mysqlCon.query(`SELECT * FROM ${prefix}posts WHERE post_type LIKE 'post' AND post_name LIKE ${postName}`, (err, rows, fields) => {
-          close();
+          close(mysqlCon);
           if (err) reject(err);
           else resolve(rows);
         });
